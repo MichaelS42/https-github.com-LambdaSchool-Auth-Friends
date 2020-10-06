bc7d8d86bdb4136a9882aeee560631320884cd2b
Michael Sanchez <michaelsanchez42@live.com>
2020-10-05 21:36:19 -0600

    mvp

--- friends/src/utils/PrivateRoute.js
+++ friends/src/utils/PrivateRoute.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-const PrivateRoute = () => ({ component: Component, ...rest }) => (
+const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
     render={props =>
       localStorage.getItem("authToken") ? (
-        <Component {...rest} />
+        <Component {...props} />
       ) : (
         <Redirect to="/login" />
       )
